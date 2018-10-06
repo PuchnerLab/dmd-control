936504aa092abe033fc3e93fde47a3cc820af4d3
Angel Mancebo <amancebo8@gmail.com>
2018-10-05 20:47:07 -0500

    Remove timer

--- draw_pixels.py
+++ draw_pixels.py
@@ -144,8 +144,6 @@ def handlekey(key):
     # for return will depend on the platform, so \n and \r are both
     # handled.
     elif key == ord('\n') or key == ord('\r'):
-        import time
-        t0 = time.time()
         DMA = cv2.remap(CAMERA, MAP1, MAP2, cv2.INTER_CUBIC)
         MODE = '(SENT)'
     elif key == ord('f'):
